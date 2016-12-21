766ad983872be435254f6c44bb223cc0fc39af0f
MxMcG <mmcgeevibes@gmail.com>
2016-12-21 15:59:30 -0800

    adds cattle image

--- projects/aaa/components/TestA/index.jsx
+++ projects/aaa/components/TestA/index.jsx
@@ -4,7 +4,6 @@ import Todos from './components';
 
 if (global.window) {
   const window = global.window;
-  console.log(config);
   render(
     <Todos window={window} config={config} />,
       global.window.document.getElementById('app'));

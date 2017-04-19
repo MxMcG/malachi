9fadd36e2632df7e51ce94038e6c470d0db1d4b9
MxMcG <mmcgeevibes@gmail.com>
2017-04-18 19:51:41 -0700

    adds missions content

--- projects/wwf/components/MissionsBlock/components.jsx
+++ projects/wwf/components/MissionsBlock/components.jsx
@@ -8,19 +8,16 @@ export default class MissionsBlock extends Component {
   }
 
   render() {
-    const ausSrc = this.props.componentContent.aus;
-    const chilSrc = this.props.componentContent.chil;
+    const ausSrc = this.props.componentContent.firstSrc;
+    const chilSrc = this.props.componentContent.secondSrc;
     const cdnImageBase = this.props.cdnImageBase;
     return (
       <div className="missionsBlock">
         <img src={ cdnImageBase + ausSrc } className="con mid"></img>
         <div className="con mid words t_left">
           <div className="contain">
-            <h2>Spreading the Good News</h2>
-            <p className="l">Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur.</p>
+            <h2>The Global Hunger Crisis</h2>
+            <p className="l">There is a global emergency that many of us don’t understand. We don’t get it because daily we're deciding what to eat for lunch, where to meet our friends for dinner, or filling our grocery basket with ingredients for our next meal. Meanwhile, one-ninth of the world’s population is going to bed hungry. And every ten seconds, a child dies of hunger related causes.</p>
             <Link to="/missions" className="t_b button solid black">View Partners</Link>
 
           </div>

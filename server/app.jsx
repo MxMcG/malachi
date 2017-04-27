b400e82eae0e041eef04b0704e467bc5d2fa79cd
MxMcG <mmcgeevibes@gmail.com>
2017-04-27 09:20:53 -0700

    adds server jsx

--- server/app.jsx
+++ server/app.jsx
@@ -92,10 +92,7 @@ app.get('*', (req, res) => {
           // title = "<title>MxMcG | Full-stack JavaScript Developer</title>";
           googleAnalytics = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-81672933-2', 'auto');ga('send', 'pageview');</script>";
           break;
-        case 'wwf':
-          // meta = "<meta name='description' content='@MxMcG is a full-stack JavaScript developer. Gradaute of UCLA and DevBootcamp S.F. Works out of San Diego, CA.'/>";
-          // title = "<title>MxMcG | Full-stack JavaScript Developer</title>";
-          googleAnalytics = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-81672933-2', 'auto');ga('send', 'pageview');</script>";
+        case 'wwf':                    
           tags = `<link rel='icon' type='image/png' href=${config.urls.cdnImageBase}westWardIcon1.png>`
           break;
         default:

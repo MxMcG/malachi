9a97e2a18a09f306f23dced345aca59bc858620d
MxMcG <mmcgeevibes@gmail.com>
2017-04-27 09:37:30 -0700

    adds apple support

--- server/app.jsx
+++ server/app.jsx
@@ -116,7 +116,11 @@ app.get('*', (req, res) => {
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
           <link href="https://fonts.googleapis.com/css?family=Bevan" rel="stylesheet">
+
+          <link rel="apple-touch-icon" href='${config.urls.cdnImageBase}westWardIcon1.png'>
           <link rel='icon' type='image/png' href='${config.urls.cdnImageBase}westWardIcon1.png'>
+          <meta name="msapplication-TileImage" href='${config.urls.cdnImageBase}westWardIcon1.png'>
+
           <script src="http://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
           <script src="https://use.fontawesome.com/22e36cff83.js"></script>
         </head>

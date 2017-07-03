c2a3b51732677afe01b9f7f9931c776f1d32539a
Dillon Marinez <dimarinez@yahoo.com>
2017-07-03 16:21:34 -0700

    footer fixed to bottom

--- projects/wwf/components/Home/components.jsx
+++ projects/wwf/components/Home/components.jsx
@@ -25,7 +25,7 @@ export default class Home extends Component {
             }
           ]}
         />
-        <SwipeSlideshowContainer / >
+        <SwipeSlideshowContainer />
         <FeaturedImagesTileContainer />
         <MissionsBlockContainer />
         <div className="wr_o">

1654548bedc0abdf305d916d18aa7a77164812ce
MxMcG <mmcgeevibes@gmail.com>
2017-12-05 16:10:39 -0800

    updates file name

--- server/configs.js
+++ server/configs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import database from './database/index.js';
 import gutil from 'gulp-util';
 import content from '../projects/wwf/content/content.json'
-import prodContent from '../projects/wwf/content/prodContent.json'
+import prodContent from '../projects/wwf/content/productionContent.json'
 
 
 export const setupConfigs = (env, activeProject, callback) => {
@@ -10,37 +10,39 @@ export const setupConfigs = (env, activeProject, callback) => {
     urls: {}
   };
   if (env === 'development') {
-    // database.fetchContentDev(activeProject, (err, data) => {
-    //   if (err) { throw err; }
-    //   // delete unwanted mongo db properties
-    //   delete data._id;
-    //   delete data.__v;
-    //   console.log(data)
-    //   config.content = data;
-    //   config.urls.cdnUrl = 'http://localhost:8080/projects/' + activeProject + '/';
-    //   config.urls.cdnImageBase = 'http://localhost:8080/projects/' + activeProject + '/images/';
-    //   config.urls.bundleUrl = 'http://localhost:8080/bundle.js/'
-    //   callback(config);
-    // });
     callback(content)
   }
 
   if (env === 'production') {
-    // database.fetchContentProd(activeProject, (err, data) => {
-    //   // delete unwanted mongo db properties
-    //   gutil.log('VERSION PRODUCTION', data.projectVersion)
-    //   const currentProjectVersion = data.projectVersion;
-    //   delete data._id;
-    //   delete data.__v;
-    //   config.projectVersion = currentProjectVersion;
-    //   config.content = data;
-    //   config.urls.cdnUrl = 'https://d3hc4gv509jw9l.cloudfront.net/projects/' + activeProject +  '_v' + currentProjectVersion + '/';
-    //   config.urls.cdnImageBase = 'https://d3hc4gv509jw9l.cloudfront.net/projects/' + activeProject +  '_v' + currentProjectVersion + '/images/';
-    //   config.bundleCssUrl = 'https://d3hc4gv509jw9l.cloudfront.net/projects/' + activeProject + '_v' + currentProjectVersion + '/index.css';
-    //   config.urls.bundleUrl = 'https://d3hc4gv509jw9l.cloudfront.net/projects/' + activeProject + '_v' + currentProjectVersion + '/bundle.js';
-    //   callback(prodContent);
-    // });
     console.log("PRODCONTENT: ", prodContent)
     callback(prodContent);
   }
 }
+
+// database.fetchContentProd(activeProject, (err, data) => {
+//   // delete unwanted mongo db properties
+//   gutil.log('VERSION PRODUCTION', data.projectVersion)
+//   const currentProjectVersion = data.projectVersion;
+//   delete data._id;
+//   delete data.__v;
+//   config.projectVersion = currentProjectVersion;
+//   config.content = data;
+//   config.urls.cdnUrl = 'https://d3hc4gv509jw9l.cloudfront.net/projects/' + activeProject +  '_v' + currentProjectVersion + '/';
+//   config.urls.cdnImageBase = 'https://d3hc4gv509jw9l.cloudfront.net/projects/' + activeProject +  '_v' + currentProjectVersion + '/images/';
+//   config.bundleCssUrl = 'https://d3hc4gv509jw9l.cloudfront.net/projects/' + activeProject + '_v' + currentProjectVersion + '/index.css';
+//   config.urls.bundleUrl = 'https://d3hc4gv509jw9l.cloudfront.net/projects/' + activeProject + '_v' + currentProjectVersion + '/bundle.js';
+//   callback(prodContent);
+// });
+
+// database.fetchContentDev(activeProject, (err, data) => {
+//   if (err) { throw err; }
+//   // delete unwanted mongo db properties
+//   delete data._id;
+//   delete data.__v;
+//   console.log(data)
+//   config.content = data;
+//   config.urls.cdnUrl = 'http://localhost:8080/projects/' + activeProject + '/';
+//   config.urls.cdnImageBase = 'http://localhost:8080/projects/' + activeProject + '/images/';
+//   config.urls.bundleUrl = 'http://localhost:8080/bundle.js/'
+//   callback(config);
+// });
